cf95be0ff51b950bdf120023890679508f714746
maheshbusireddy <busireddymahesh@gmail.com>
2023-07-21 11:43:26 +0530

    added new files

--- frontend.sh
+++ frontend.sh
@@ -8,7 +8,7 @@ curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend.z
 cd /usr/share/nginx/html 
 unzip /tmp/frontend.zip
 
-cp nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
+cp /nginx-roboshop.conf /etc/nginx/default.d/roboshop.conf
 
 
 systemctl restart nginx 
